3de529daaca84b3ce1b13da97311593a05a71a10
Paul Vega <paulmax951@gmail.com>
2021-12-31 17:56:52 -0500

    update

---
+++ src/app/modules/admin/setting/unid/unid.routing.ts
@@ -0,0 +1,10 @@
+import { Route } from '@angular/router';
+import { BrandComponent } from './brand.component';
+
+export const unidRoutes: Route[] = [
+    {
+        path     : '',
+        component: BrandComponent,
+
+    }
+];
